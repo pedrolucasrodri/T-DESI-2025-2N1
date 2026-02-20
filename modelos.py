7050f6ed1fcde711e4b1bcdacd78f9c954942434
pedrolucasrodri <pedrolr2060@gmail.com>
2026-02-19 21:21:25 -0300

    Modelagem

---
+++ modelos.py
@@ -0,0 +1,26 @@
+"""
+Módulo modelos.py
+Definição das classes do sistema.
+
+Criado em: 19/02/2026
+Autor: [Pedro Lucas Rodrigues]
+
+Nome da Classe:
+
+alergia
+nome_paciente
+tem_convenio
+telefone 
+email
+idade 
+
+Comportamentos
+
+alergia - Para não dar o remédio errado.
+Nome_paciente - Para identificar o paciente.
+Tem_convenio - Para descontos e priorizar atendimento.
+telefone - para contato da pessoa.
+email - para enviar os resultados dos exames.
+idade - para definir prioridade de atendimento, e qual abordagem usar, com medicamentos e cirurgias.
+
+"""
